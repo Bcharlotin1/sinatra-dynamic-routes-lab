0e6451641dc3bddf84ad13f44f7e0badfbb8e71b
bcharlotin1 <biancacharlotin1@gmail.com>
2021-03-08 18:19:29 -0500

    Done.

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require_relative 'config/environment'
-
+require 'pry'
 class App < Sinatra::Base
   # Write your code here!
   get '/reversename/:name' do
@@ -18,8 +18,12 @@ class App < Sinatra::Base
     number = params[:number]
     @phrase = params[:phrase]
     n = number.to_i
-    n.times.collect {"#{@phrase}"}.inject(:+)
-    
+    @string = []
+    n.times do 
+      @string << @phrase
+    end
+    "#{@string.join(",")}"
+
   end
 
   get "/say/:word1/:word2/:word3/:word4/:word5" do
@@ -28,23 +32,31 @@ class App < Sinatra::Base
     word3 = params[:word3]
     word4 = params[:word4]
     word5 = params[:word5]
-   "#{word1} + #{word2} + #{word3} + #{word4} + #{word5} + ."
+   word = "#{word1}" + " #{word2}" + " #{word3}" + " #{word4}" + " #{word5}" + "."
+
+  #  @string = ""
+  #  @string.concat("#{word1} ")
+  #  @string.concat(" #{word2} ")
+  #  @string.concat(" #{word3} ")
+  #  @string.concat(" #{word4} ")
+  #  @string.concat(" #{word1} ")
+  #  @string.concat(".")
   end
   get "/:operation/:number1/:number2" do
     operation = params[:operation]
     num1 = params[:number1].to_i
     num2 = params[:number2].to_i
     @solution = nil
-    if operation = "add"
-      @solution = num1 + num2
-    elsif operation = "subtract"
-      @solution = num2 - num1
-    elsif operation = "multiply"
+    if operation =="add"
+      @solution = num1. + num2
+    elsif operation == "subtract"
+      @solution = num1 - num2
+    elsif operation == "multiply"
       @solution = num1 * num2
-    elsif operation = "divide"
-      @solution = num1 / num2
+    elsif operation == "divide"
+      @solution = num1.to_f / num2.to_f
     end
-    "#{@solution}"
+    "#{@solution.to_s}"
   end
 
 
